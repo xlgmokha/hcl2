2473a75913cd70f2a709fa5547aee5bef2eca301
mo khan <mo@mokhan.ca>
2021-06-05 13:03:57 -0600

    feat: port HCL2 parser from spandx
    * https://github.com/spandx/spandx/blob/dc7ea719adbf2e90753cb92be76e21cddc916557/lib/spandx/terraform/parsers/hcl.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require "hcl2"
+require "parslet/convenience"
+require "parslet/rig/rspec"
+
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
---
+++ spec/support/files.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include(Module.new do
+    def fixture_file(path)
+      Pathname.new(__FILE__).parent.join('../fixtures', path)
+    end
+
+    def fixture_file_content(path)
+      fixture_file(path).read
+    end
+  end)
+end
